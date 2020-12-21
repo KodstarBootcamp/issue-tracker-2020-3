dca5cd7be761ec80fe607ced5d7eb7afa957bc63
celil.tat <23celiltat61@gmail.com>
2020-12-21 02:11:44 +0100

    added modules folder and routes folder, post send successfully and backend connected with database.

---
+++ backend/models/issue.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const issueSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  labels: { type: Array, required: true}
+})
+
+const Issue = mongoose.model('Issue', issueSchema)
+module.exports = Issue
---
+++ backend/routes/issues.js
@@ -0,0 +1,30 @@
+const router = require('express').Router()
+let Issue = require('../models/issue.model')
+
+router.route('/').get((req, res) => {
+  Issue.find()
+    .then(issues => res.json(issues))
+    .catch(err => res.status(400).json('Error: ' + err))
+})
+
+router.route('/create').post((req, res) => {
+  const title = req.body.title
+  const description = req.body.description
+  const labels = req.body.labels
+
+  const newIssue = new Issue({
+    title,
+    description,
+    labels
+  })
+
+  newIssue.save()
+    .then(() => res.json('Successful Operation!'))
+    .catch(err => res.status(400).json('Error: ' + err))
+})
+
+module.exports = router
+
+
+
+
