bd0d56a58438971d7569aa48365d6ddb6d582701
yuskara <57836403+yuskara@users.noreply.github.com>
2020-12-24 04:51:18 -0500

    Update Issue.js
    deleted comment outs

--- web/src/components/Issue.js
+++ web/src/components/Issue.js
@@ -18,32 +18,7 @@ const Issue=({issue, handleDelete}) => {
               <Card.Text>{issue.labels[0]}</Card.Text>
               <Button onClick={ () => setView(true)}>Edit </Button>
               <Button onClick={ () => handleDelete(issue.id)} variant="danger"  >Delete</Button>
-              {/*
- <CardColumns> Card Colums-1 for try
-                <Card border="primary" style={{ width: '18rem' }}>
-                <Card.Header as="h5">{issue.title}</Card.Header>
-                <Card.Body>
-                    <Card.Title></Card.Title>
-                    <Card.Text> {issue.description} </Card.Text>
-                    <Card.Text>{issue.labels}</Card.Text>
-                    <Button onClick={ () => handleEdit(issue.id)}>Edit </Button>
-                    <Button onClick={ () => handleDelete(issue.id)} variant="danger"  >Delete</Button>
-                  </Card.Body>
-                </Card>
-            </CardColumns>
-            <CardColumns> Card Colums -2 for try
-                <Card border="primary" style={{ width: '18rem' }}>
-                <Card.Header as="h5">{issue.title}</Card.Header>
-                <Card.Body>
-                    <Card.Title></Card.Title>
-                    <Card.Text> {issue.description} </Card.Text>
-                    <Card.Text>{issue.labels}</Card.Text>
-                    <Button onClick={ () => handleEdit(issue.id)}>Edit </Button>
-                    <Button onClick={ () => handleDelete(issue.id)} variant="danger"  >Delete</Button>
-                  </Card.Body>
-                </Card>
-            </CardColumns>
-              */
+              
            }
              
             </Card.Body>
@@ -51,32 +26,8 @@ const Issue=({issue, handleDelete}) => {
             </Card>
       </CardDeck>
       </div>
-    /*
-      <tr>
-            <td>{issue.title} </td>
-            <td>{issue.description}</td>
-            <td>{issue.labels}
-            <td>xx</td><td>yyy </td>
-            </td>
-            <td><Button onClick={ () => handleEdit(issue.id)}>Edit </Button></td>
-            <td><Button onClick={ () => handleDelete(issue.id)} variant="danger"  >Delete</Button></td>
-      </tr>
-      */
+ 
   );
 }
 
 export default Issue;
-
-/*
-
-menüler
-home  new 
-
--------------x edit delete
------------ edit delete 
--------------- edit delete 
-
-edit-update
-delete
-
-*/
