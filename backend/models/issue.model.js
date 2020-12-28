20ecce9f5e15269cf05b2955abf7da2a1c8511a1
celil.tat <23celiltat61@gmail.com>
2020-12-28 19:42:33 +0100

    added new codes for date-information

--- backend/models/issue.model.js
+++ backend/models/issue.model.js
@@ -5,7 +5,10 @@ const Schema = mongoose.Schema
 const issueSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  labels: { type: Array, required: true }
+  labels: { type: Array, required: true },
+  date: { type: Date, required: true }
+}, {
+  timestamps: true
 })
 
 issueSchema.set('toJSON', {
--- backend/routes/issues.js
+++ backend/routes/issues.js
@@ -8,6 +8,8 @@ router.route('/').post(async (req, res) => {
   const title = req.body.title
   const description = req.body.description
   const labels = req.body.labels
+  const date = Date.parse(req.body.date)
+
   const check = await Issue.findOne({ title:req.body.title })
   if(check){
     return res.status(409).send('Issue already exist').end()
@@ -15,7 +17,8 @@ router.route('/').post(async (req, res) => {
   const newIssue = new Issue({
     title,
     description,
-    labels
+    labels,
+    date
   })
   const savedIssue = await newIssue.save()
   return res.status(200).json(savedIssue)
@@ -54,6 +57,7 @@ router.route('/:id').put(async (req, res) => {
     title:req.body.title,
     description:req.body.description,
     labels:req.body.labels,
+    date: Date.parse(req.body.date)
   }
   objCleaner(newIssue)
   const savedIssue = await Issue.findByIdAndUpdate(req.params.id, newIssue, { new:true })
