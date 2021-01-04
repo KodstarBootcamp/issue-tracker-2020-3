cea0631fef66594ba047ebff33d0cfbcdefb26d9
Burak-Esen <0.burak.esen@gmail.com>
2021-01-04 22:30:13 +0100

    get all issue api - sort parameter added

--- backend/routes/issues.js
+++ backend/routes/issues.js
@@ -46,31 +46,21 @@ router.route('/').post(async (req, res) => {
 
 // ↓↓↓ this route must be top cause of '/all' - ':id' conflict
 router.route('/all').get(async (req, res) => {
-  const issues = await Issue.find({}).populate('labels')
-  res.status(200).json(issues).end()
-})
-
-router.route('/paginate/:slice').get(async (req, res) => {
-  const slice = String(req.params.slice)
-  const sliceHasDash = slice.includes('-')
-  let skip
-  let limit
-  if (sliceHasDash) {
-    skip = Number.parseInt(slice.split('-')[0])
-    limit = Number.parseInt(slice.split('-')[1]) - skip
-  } else {
-    skip = 0
-    limit = Number.parseInt(slice)
+  const sortTypes = [
+    'createdAt', 'updatedAt','title',
+    '-createdAt', '-updatedAt','-title'
+  ]
+  if (req.query.sort && !sortTypes.includes(req.query.sort)){
+    return res.status(405).send('unavailable type of sort').end()
   }
-  const issues = await Issue.find({}, null, { skip, limit }).populate('labels')
-  res.status(200).json(issues)
-})
-router.route('/:id').get(async (req, res) => {
-  const issue = await Issue.findById(req.params.id).populate('labels')
-  if (!issue){
-    return res.status(404).send('Issue not found').end()
+  if (!req.query.start && !req.query.count) {
+    const issues = await Issue.find({}).sort(req.query.sort).populate('labels')
+    return res.status(200).json(issues).end()
   }
-  return res.status(200).json(issue)
+  const skip = Number.parseInt(req.query.start) || 0
+  const limit = Number.parseInt(req.query.count) || 10
+  const issues = await Issue.find({}, null, { skip, limit }).sort(req.query.sort).populate('labels')
+  return res.status(200).json(issues).end()
 })
 
 router.route('/:id').delete(async (req, res) => {
