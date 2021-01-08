09084a382ac7a51541e955ff1dbd46aad147c879
Mustafa <cana66331@gmail.com>
2021-01-08 10:21:31 +0100

    some code cleaned

--- web/src/components/labels/LabelDetails.test.js
+++ web/src/components/labels/LabelDetails.test.js
@@ -3,24 +3,6 @@ import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react'
 import { LabelDetails } from './LabelDetails'
 
-/*
-// method 1
-  expect(component.container).toHaveTextContent(
-    'Component testing is done with react-testing-library'
-  )
-
-  // method 2
-  const element = component.getByText(
-    'Component testing is done with react-testing-library'
-  )
-  expect(element).toBeDefined()
-
-  // method 3
-  const div = component.container.querySelector('.note')
-  expect(div).toHaveTextContent(
-    'Component testing is done with react-testing-library'
-
-*/
 
 describe('<Label test', () => {
   const LabelTest = {
