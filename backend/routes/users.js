f06212bf9a2d244793aba94a0e7ded88b040c38e
Burak-Esen <0.burak.esen@gmail.com>
2021-01-08 02:51:15 +0100

    merged with main

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,22 +1,20 @@
 const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user.model')
-const mongoose = require('mongoose')
 
 usersRouter.post('/', async (req, res) => {
   const body = req.body
 
   if (!body.password || !body.username) {
-    return res.status(401).send({ error: 'password or username missing' })
+    return res.status(401).send('password or username missing')
   } else if (body.password.length < 3) {
-    return res.status(402).send({ error: 'password should be at 3 characters long' })
+    return res.status(402).send('password should be at 3 characters long')
   }
 
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
   const user = new User({
-    _id: new mongoose.Types.ObjectId(),
     username: body.username,
     passwordHash,
     email: body.email
@@ -30,7 +28,7 @@ usersRouter.post('/', async (req, res) => {
 usersRouter.get('/', async(req, res) => {
   const users = await User
     .find({})
-    .populate('issues', { title: 1, author: 1, url: 1, id: 1 })
+    .populate('issues', { title: 1, id: 1 })
   res.json(users)
 })
 
