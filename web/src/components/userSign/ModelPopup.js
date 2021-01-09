b63dd01a3807e002e09e1d46a70692bf8674b370
Mustafa <cana66331@gmail.com>
2021-01-09 19:32:38 +0100

    fix updated

--- web/src/components/userSign/ModelPopup.js
+++ web/src/components/userSign/ModelPopup.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import { Button } from 'reactstrap'
 
-const ModelPopup =() => {
+const ModelPopup =(props) => {
   return (
-
     <div className='popup'>
-
       <div className='popup-inner'>
-
-        <h1>{this.props.text}</h1>
-        <Button color='primary' onClick={this.props.closePopup}>close me</Button>
+        <h1>{props.text}</h1>
+        <Button color='primary' onClick={props.closePopup}>close me</Button>
       </div>
     </div>
   )
