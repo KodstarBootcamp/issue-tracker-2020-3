152b6f443f00baa40313d11133ac32825edde0da
Mustafa <cana66331@gmail.com>
2021-01-13 14:20:00 +0100

    assign style added

--- web/src/components/issues/IssueCreateForm.js
+++ web/src/components/issues/IssueCreateForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Form, Button,ButtonToolbar,ButtonGroup, Modal } from 'react-bootstrap'
+import { Form, Button,  Modal } from 'react-bootstrap'//ButtonToolbarButtonGroup
 import { LabelSelect } from '../labels'
 import { LabelCreateForm } from '../labels'
 
@@ -38,57 +38,57 @@ export const IssueCreateForm = ( props ) => {
   return (
     <div className="form">
       <h2>Create a new issue</h2>
-      <Form onSubmit={addIssue}>
-        <Form.Group>
-          <Form.Label>title:</Form.Label>
-          <Form.Control
-            required
-            id="title"
-            type="text"
-            name="title"
-            value={title}
-            onChange={({ target }) => setTitle(target.value)}
-          />
-          <Form.Label>description:</Form.Label>
-          <Form.Control
-            as="textarea"
-            required
-            id="description"
-            type="text"
-            name="description"
-            value={description}
-            onChange={({ target }) => setDescription(target.value)}
-          />
-          <Form.Label>Labels:</Form.Label>
-          <div>
-            {props.option?<LabelSelect style={styles.select} option={props.option} isMulti={true}  onChange={onChangeInput}/>:''}
-          </div>
-          <ButtonToolbar aria-label="Toolbar with button groups">
-            <ButtonGroup className="mr-2" aria-label="First group">
+      <div className='d-flex'>
+
+        <div className='p-2 mr-auto flex-fill'>
+          <Form onSubmit={addIssue}>
+            <Form.Group>
+              <Form.Label>title:</Form.Label>
+              <Form.Control
+                required
+                id="title"
+                type="text"
+                name="title"
+                value={title}
+                onChange={({ target }) => setTitle(target.value)}
+              />
+              <Form.Label>description:</Form.Label>
+              <Form.Control
+                as="textarea"
+                required
+                id="description"
+                type="text"
+                name="description"
+                value={description}
+                onChange={({ target }) => setDescription(target.value)}
+              />
               < Button id="createButton" type="submit" variant="primary">create new issue</Button>
-            </ButtonGroup>
-            <ButtonGroup className="mr-2" aria-label="Second group">
-              <Button variant="success"  onClick={handleClick}>create label</Button>
-            </ButtonGroup>
-          </ButtonToolbar>
-        </Form.Group>
-      </Form>
-      <Modal
-        size="sm"
-        show={smShow}
-        onHide={() => setSmShow(false)}
-        aria-labelledby="example-modal-sizes-title-sm"
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="example-modal-sizes-title-sm">
+            </Form.Group>
+          </Form>
+        </div >
+        <div className='p-2 mr-auto flex-fill'>
+          <br /><Button variant="primary"  onClick={handleClick}>assign</Button><br />
+          <Form.Label>Labels:</Form.Label>
+          {props.option?<LabelSelect style={styles.select} option={props.option} isMulti={true}  onChange={onChangeInput}/>:''}<br />
+          <Button variant="success"  onClick={handleClick}>create label</Button>
+        </div>
+        <Modal
+          size="sm"
+          show={smShow}
+          onHide={() => setSmShow(false)}
+          aria-labelledby="example-modal-sizes-title-sm"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="example-modal-sizes-title-sm">
             Create New Label
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <LabelCreateForm setViewIssueEdit={props.setViewIssueEdit} setLabelSelect={props.setLabelSelect} labelSelect={props.labelSelect} addLabel={props.addLabel}
-            setSmShow={setSmShow}/>
-        </Modal.Body>
-      </Modal>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <LabelCreateForm setViewIssueEdit={props.setViewIssueEdit} setLabelSelect={props.setLabelSelect} labelSelect={props.labelSelect} addLabel={props.addLabel}
+              setSmShow={setSmShow}/>
+          </Modal.Body>
+        </Modal>
+      </div>
     </div>
   )
 }
